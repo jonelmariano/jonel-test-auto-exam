6f6a466501ef8cdce7cae194192892779a1b2f0b
jonelmariano <jonelmariano.freelancing@gmail.com>
2024-05-28 21:36:45 +0800

    Create BasePage.js and LoginPage.js

---
+++ cypress/pages/BasePage.js
@@ -0,0 +1,9 @@
+class basePage{
+
+elements = {
+newButton: () =>cy.get('span.ant-btn-icon'),
+
+
+
+}
+}
---
+++ cypress/pages/login/LoginPage.js
@@ -0,0 +1,9 @@
+class loginPage{
+
+    elements = {
+    emailTextBox: () => cy.get('input[type="email"]'),
+    passwordTextBox: () => cy.get('input[type="password"]'), 
+    loginButton: () => cy.get('button[type="submit"]'),
+    forgotPassword: () => cy.get('a[href="/password-reset/"]') 
+    }
+    }
